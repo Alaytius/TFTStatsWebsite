e6666e441d848576ed27190ba4b0f6ea31eeff1d
Alaytius <t.chen1459@gmail.com>
2025-01-30 20:42:10 -0800

    Testing

--- api/main.js
+++ api/main.js
@@ -28,11 +28,31 @@ let table = new DataTable('#stats', {
 });
 
 app.get('/', (req, res) => {
-  fs.readFile('index.html', 'utf8', (err, data) => {
-    if (err) throw err;
-    res.send(data);
-    console.log(stats);
-  });
+  html = `<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TFT Augment Stats</title>
+</head>
+<body>
+    <h1 style="text-align:center">TFT Augment Stats</h1>
+    <table id="stats" style="width:70%; text-align:center;">
+        <thead>
+            <tr>
+                <th>Augment Name</th>
+                <th>Average Placement</th>
+                <th>2-1</th>
+                <th>3-2</th>
+                <th>4-2</th>
+                <th>Games Played</th>
+            </tr>
+    </table>
+</body>`;
+  // fs.readFile('index.html', 'utf8', (err, data) => {
+  //   if (err) throw err;
+  //   res.send(data);
+  //   console.log(stats);
+  // });
+  res.send(data)
 });
 
 
