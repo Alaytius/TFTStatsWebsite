c968224010005a4f38846f7626b4a9696af837c7
Alaytius <t.chen1459@gmail.com>
2025-02-01 09:52:39 -0800

    Testing

--- api/main.js
+++ api/main.js
@@ -5,29 +5,29 @@ const DataTable = require('datatables.net');
 // import fs from 'node:fs';
 const app = express();
 
-const requestOptions = {
-  method: 'GET',
-  headers: {
-    'X-API-KEY': `${process.env.APIKEY}`,
-  },
-};
+// const requestOptions = {
+//   method: 'GET',
+//   headers: {
+//     'X-API-KEY': `${process.env.APIKEY}`,
+//   },
+// };
 
-const stats = await fetch(process.env.APIURL, requestOptions)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    console.log(response.json());
-    return response.json();
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// const stats = await fetch(process.env.APIURL, requestOptions)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     console.log(response.json());
+//     return response.json();
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
 
-let table = new DataTable('#stats', {
-    data: stats,
-    paging: false
-});
+// let table = new DataTable('#stats', {
+//     data: stats,
+//     paging: false
+// });
 
 // app.get('/', (req, res) => {
 //   html = `<head>
