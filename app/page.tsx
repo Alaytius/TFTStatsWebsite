dafdb1fda6d958a5d395841283563d6ba260c0a1
Alaytius <t.chen1459@gmail.com>
2025-02-06 19:19:52 -0800

    next.js

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import { Augments, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+
+// const requestOptions = {
+//   method: 'GET',
+//   headers: {
+//     'X-API-KEY': `${process.env.APIKEY}`,
+//   },
+// };
+
+// async function getData(): Promise<Augments[]> {
+//   const data = await fetch(`http://${process.env.APIURL}/${process.env.ENDPOINT}`);
+//   const stats = await data.json();
+//   return stats
+// };
+export const revalidate = 120
+
+export default async function Home() { 
+  const res = await fetch(`${process.env.APIURL}`)
+  const data: Augments[] = await res.json()
+  console.log(data)
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="title">TFT Augment Stats</h1>
+      <h1>Patch 13.5</h1>
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
