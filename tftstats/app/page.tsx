5c80b8f180c8c95502f7f659070217dd71bc4a7c
Alaytius <t.chen1459@gmail.com>
2025-02-06 19:11:29 -0800

    next.js

--- tftstats/app/page.tsx
+++ tftstats/app/page.tsx
@@ -17,7 +17,7 @@ import { DataTable } from "./data-table";
 export const revalidate = 120
 
 export default async function Home() { 
-  const res = await fetch(`http://jsonplaceholder.typicode.com/posts`)
+  const res = await fetch(`${process.env.APIURL}`)
   const data: Augments[] = await res.json()
   console.log(data)
   return (
