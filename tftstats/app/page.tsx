90a5d83726ee8f7a302a510f79eb949a0f500790
Alaytius <t.chen1459@gmail.com>
2025-02-06 19:05:05 -0800

    next.js

--- tftstats/app/page.tsx
+++ tftstats/app/page.tsx
@@ -1,6 +1,5 @@
 import { Augments, columns } from "./columns";
 import { DataTable } from "./data-table";
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 
 
 // const requestOptions = {
@@ -15,19 +14,16 @@ import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 //   const stats = await data.json();
 //   return stats
 // };
-export const getServerSideProps = (async () => {
-  const res = await fetch(`http://${process.env.APIURL}/${process.env.ENDPOINT}`);
-  const data: Augments[] = await res.json();
-  return { props: { data }}
-}) satisfies GetServerSideProps<{data: Augments[]}>
+export const revalidate = 3600
 
-
-export default function Home({data,}:InferGetServerSidePropsType<typeof getServerSideProps>) { 
+export default async function Home() { 
+  const res = await fetch(`http://${process.env.APIURL}/${process.env.ENDPOINT}`)
+  const data: Augments[] = await res.json()
   return (
     <div className="container mx-auto py-10">
       <h1 className="title">TFT Augment Stats</h1>
       <h1>Patch 13.5</h1>
       <DataTable columns={columns} data={data} />
     </div>
-  );
+  )
 }
