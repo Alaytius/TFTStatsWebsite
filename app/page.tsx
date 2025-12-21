acbc44b26bad84b73f73aa6ba96005e8afcda097
Alaytius <t.chen1459@gmail.com>
2025-12-21 14:13:57 -0800

    patch update

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   return (
     <div className="container mx-auto py-10">
       <h1 className="title">TFT Augment Stats by @Setsuko VODS</h1>
-      <h1>Patch 16.1</h1>
+      <h1>Patch 16.1c</h1>
       <h1>Data from master+ games, updates every 30 minutes</h1>
       <DataTable columns={columns} data={data} />
       <Analytics />
